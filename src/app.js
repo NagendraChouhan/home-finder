bb2b78c9ae432eee4905f4e202da8746fa59a09f
Nagendra Chouhan <nikku200109@gmail.com>
2022-07-10 22:47:11 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, '../frontEnd/public')))
+// app.use(express.static(path.join(__dirname, '../frontEnd/public')))
 
 // app.get('*', function(req, res) {
 //   res.sendFile(path.join(__dirname, '../frontEnd/public', 'index.html'))
