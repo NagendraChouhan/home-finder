1d4ab0026b2aca4093ddd6d5731e546a4a2c1357
Nagendra Chouhan <nikku200109@gmail.com>
2022-07-10 21:48:38 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const path = require('path');
 const cors = require('cors');
 require('./database/connection');
 const bcryptjs = require('bcryptjs');
