ed50d7dcb6f17af57a96ef88079733071f9e0f0a
Nagendra Chouhan <nikku200109@gmail.com>
2022-07-10 21:23:59 +0530

    Update package.json

--- src/app.js
+++ src/app.js
@@ -31,11 +31,11 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 app.use('/logout',logout)
 app.use('/login',login)
